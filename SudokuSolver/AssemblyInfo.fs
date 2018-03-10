88ea1fd5b3a3d35d8643174c83f0ec0b2f6c09ba
Shridhara Rangaraju <mrshridhara@outlook.com>
2018-03-10 14:09:10 +0530

    Build improvements

--- SudokuSolver/AssemblyInfo.fs
+++ SudokuSolver/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<AssemblyProduct("Su-Do-Ku Solver")>]
 [<AssemblyCompany("Shridhara Rangaraju")>]
 [<AssemblyCopyright("Copyright © 2018 Shridhara Rangaraju")>]
-[<AssemblyVersion("1.0.0.0")>]
-[<AssemblyFileVersion("1.0.0.0")>]
-[<AssemblyInformationalVersion("1.0.0.0")>]
+[<AssemblyVersion("1.0.0.*")>]
+[<AssemblyFileVersion("1.0.0")>]
+[<AssemblyInformationalVersion("1.0.0")>]
 
 do()
